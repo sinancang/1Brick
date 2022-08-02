866346f2b6124cb5ac311899a2079d9b74bb7383
sinancang <sinancangulan@gmail.com>
2022-08-01 21:23:28 -0400

    1Brick - refactor

--- main.py
+++ main.py
@@ -23,25 +23,10 @@ class MainWindow(QMainWindow):
         self.calendar.setObjectName(u"calendarWidget")
         self.calendar.setMinimumSize(QSize(length, height))
         self.calendar.setMaximumSize(length, height)
-        self.calendar.setStyleSheet(open("calendarStyle.css").read())
+        self.calendar.setStyleSheet(open("StyleSheets/calendarStyle.css").read())
         self.calendar.setGridVisible(True)
 
         self.ui.gridLayout.addWidget(self.calendar, 0, 0, 1, 1, Qt.AlignHCenter | Qt.AlignTop)
-
-    def createHabits(selfs):
-        length = 600
-        height = 400
-
-    def createNewWidget(self, rowNumber, columnNumber):
-        self.frame = QFrame(self.ui.scrollAreaWidgetContents)
-        self.frame.setObjectName(u"frame")
-        self.frame.setMinimumSize(QSize(80, 80))
-        self.frame.setMaximumSize(QSize(80, 80))
-        self.frame.setStyleSheet(u"background-color: rgb(255, 212, 168);")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-
-        self.ui.gridLayout.addWidget(self.frame, rowNumber, columnNumber, 1, 1, Qt.AlignHCenter | Qt.AlignVCenter)
 
 
 def main():
